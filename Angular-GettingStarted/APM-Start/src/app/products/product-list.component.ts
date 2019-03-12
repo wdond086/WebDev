68a7c97a5c23cf1a5380415e069a0468d97de492
Wilfried Dondji <tdondjiwilfried@hotmail.com>
2019-03-12 06:29:43 -0400

    Nested components implemented

--- Angular-GettingStarted/APM-Start/src/app/products/product-list.component.ts
+++ Angular-GettingStarted/APM-Start/src/app/products/product-list.component.ts
@@ -92,4 +92,8 @@ export class ProductListComponent implements OnInit {
         return this.products.filter((product: IProduct) =>
                 product.productName.toLowerCase().indexOf(filterBy) !== -1);
       }
+
+      onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+      }
 }
